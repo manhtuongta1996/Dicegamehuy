e4471d5f5a523b8fa1aa485840129b7d20352f9b
manhtuongta1996 <37558085+manhtuongta1996@users.noreply.github.com>
2020-12-20 19:04:40 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// document.querySelector(".play").onclick = function(){
-//     var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-//     var sourceRandomNumber1 = "images/dice" + randomNumber1 + ".png";
-//     document.querySelector(".img1").setAttribute("src", sourceRandomNumber1);
 
-//     var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-//     var sourceRandomNumber2 = "images/dice" + randomNumber2 + ".png";
-//     document.querySelector(".img2").setAttribute("src", sourceRandomNumber2);
-
-//     if (randomNumber1 > randomNumber2) {
-//         document.querySelector("h1").innerHTML = "🥊 Player 1 wins";
-//     } else if (randomNumber1 < randomNumber2) {
-//         document.querySelector("h1").innerHTML = "Player 2 wins 🥊";
-//     } else {
-//         document.querySelector("h1").innerHTML = " 🤦 Drawwww!!!";
-//     }
-// }
 function play(str){
 
     var randomNumber1 = Math.floor(Math.random() * 6 + 1);
